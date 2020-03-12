f4bafc13fcf989d94978f395c1162dabe237dddc
uzair11 <uzairimtiaz44@gmail.com>
2020-03-12 23:10:57 +0500

    sign up

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,4 @@
 export const BASE_URL = 'https://animals-interview-backend.herokuapp.com/'
 export const LOGIN_URL = 'api/auth/login/'
+export const SIGNUP_URL = 'api/auth/register/'
 export const ANIMALS_URL = 'api/animal/'
--- src/Login.js
+++ src/Login.js
@@ -7,53 +7,91 @@ class LoginPage extends Component {
 		super(props)
 
 		this.state = {
-			email: '',
-			password: '',
-			error:'',
+			loginEmail: '',
+			loginPassword: '',
+			loginError:'',
+			signupEmail: '',
+			signupPassword: '',
+			signupMessage: '',
 		}
 	}
 
 	render(){
 		return(
 			<div className="App">
-		      <h1>Login Page</h1>
-		        <label>Email:</label>
-		        <input type="text" name="email" id="email" onChange={this.onEmailChange} value={this.state.email}/><br/><br/>
-		        <label>Password:</label>
-		        <input type="text" name="password" id="password" onChange={this.onPasswordChange} value={this.state.password}/><br/><br/>
-		        <p>{this.state.error}</p>
-		        <input type="submit" value="Submit" onClick={this.attemptLogin}/>
-					<br/><br/>
+			  <div className="Login">
+		        <h1>Login</h1>
+                <label>Email:</label>
+                <input type="text" name="email" id="email" onChange={this.onLoginEmailChange} value={this.state.loginEmail}/><br/><br/>
+                <label>Password:</label>
+                <input type="text" name="password" id="password" onChange={this.onLoginPasswordChange} value={this.state.loginPassword}/><br/><br/>
+                <p>{this.state.loginError}</p>
+                <input type="submit" value="Submit" onClick={this.attemptLogin}/>
+                <br/><br/>
+			  </div>
+			  <div className="Signup">
+                <h1>Signup</h1>
+                <label>Email:</label>
+                <input type="text" name="email" id="email" onChange={this.onSignupEmailChange} value={this.state.signupEmail}/><br/><br/>
+                <label>Password:</label>
+                <input type="text" name="password" id="password" onChange={this.onSignupPasswordChange} value={this.state.signupPassword}/><br/><br/>
+                <p>{this.state.signupMessage}</p>
+                <input type="submit" value="Submit" onClick={this.attemptSignup}/>
+                <br/><br/>
+			  </div>
 		    </div>
 
 		);
 	}
 
-	onEmailChange = (e) => {
-		this.setState({email: e.target.value});
+	onLoginEmailChange = (e) => {
+		this.setState({loginEmail: e.target.value});
 	}
 
-	onPasswordChange = (e) => {
-		this.setState({password: e.target.value});
+	onLoginPasswordChange = (e) => {
+		this.setState({loginPassword: e.target.value});
+	}
+
+	onSignupEmailChange = (e) => {
+		this.setState({signupEmail: e.target.value});
+	}
+
+	onSignupPasswordChange = (e) => {
+		this.setState({signupPassword: e.target.value});
 	}
 
 	attemptLogin = (e) => {
       axios.post(CONSTANTS.BASE_URL + CONSTANTS.LOGIN_URL, {
-          email: this.state.email,
-	    		password: this.state.password,
+          email: this.state.loginEmail,
+	    		password: this.state.loginPassword,
         })
         .then((response) => {
           console.log(response);
-          this.setState({error: ''})
+          this.setState({loginError: ''})
           sessionStorage.setItem('token',response.data.token)
           this.props.setUserProps(response.data.token, response.data.id, response.data.email)
         })
         .catch((error) => {
           console.log(error);
-          this.setState({error: 'Login failed. Please check email and credentials and try again.'})
+          this.setState({loginError: 'Login failed. Please check email and credentials and try again.'})
       });
 	}
 
+	attemptSignup = (e) => {
+      axios.post(CONSTANTS.BASE_URL + CONSTANTS.SIGNUP_URL, {
+          email: this.state.signupEmail,
+	      password: this.state.signupPassword,
+        })
+        .then((response) => {
+          console.log(response);
+          this.setState({signupMessage: 'Signup Successful. Please login to continue'})
+
+        })
+        .catch((error) => {
+          console.log(error);
+          this.setState({signupMessage: 'Signup failed. Please ensure entering correct email address.'})
+      });
+	}
 }
 
 export default LoginPage;
